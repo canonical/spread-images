3f88861efc5914d21b6b46c1d762363ca8c55bf3
sergio-j-cazzolato <sergio.cazzolato@canonical.com>
2018-03-27 09:30:48 -0300

    Moving functions to google and making them more robust

--- lib/google.sh
+++ lib/google.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-snapshot(){
-    DISK=$1
-    TEMPORAL_PATH=/tmp/spread
-    if [ -e $TEMPORAL_PATH ]; then
-        rm -rf $TEMPORAL_PATH
-    fi
-    mv $PROJECT_PATH $TEMPORAL_PATH
-    gcloud compute disks snapshot $DISK --zone $ZONE --snapshot-names $DISK
-    mv $TEMPORAL_PATH $PROJECT_PATH
+create_image_from_snapshot(){
+    IMAGE=$1
+    FAMILY=$2
+    DESCRIPTION=$3
+    SNAPSHOT=$4
+    gcloud compute images create "$IMAGE" --family "$FAMILY" --description "$DESCRIPTION" --source-snapshot "$SNAPSHOT"
 }
 
-deprecate (){
+delete_image(){
+    IMAGE=$1
+    image_name=$(gcloud compute images list --project $GCE_PROJECT --filter "family = $FAMILY AND name = $IMAGE" --format json | jq -r '.[]|.name')
+
+    if ! -z $image_name; then
+        gcloud compute images delete "$IMAGE"
+    fi
+}
+
+deprecate_old_images (){
   FAMILY=$1  
   latest_image_name=$(gcloud compute images describe-from-family $FAMILY --project $GCE_PROJECT --format json | jq -r '.name')
   echo "Latest Image: ${latest_image_name}"
@@ -27,4 +33,24 @@ deprecate (){
       echo ""
     done
   fi
+}
+
+create_snapshot_from_disk(){
+    DISK=$1
+    TEMPORAL_PATH=/tmp/spread
+    if [ -e $TEMPORAL_PATH ]; then
+        rm -rf $TEMPORAL_PATH
+    fi
+    mv $PROJECT_PATH $TEMPORAL_PATH
+    gcloud compute disks snapshot $DISK --zone $ZONE --snapshot-names $DISK
+    mv $TEMPORAL_PATH $PROJECT_PATH
+}
+
+delete_snapshot(){
+    SNAPSHOT=$1
+    snapshot_name=$(gcloud compute snapshots list --project $GCE_PROJECT --filter "name = $SNAPSHOT" --format json | jq -r '.[]|.name')
+
+    if ! -z $snapshot_name; then
+        gcloud compute snapshots delete "$SNAPSHOT" --quiet
+    fi
 }
