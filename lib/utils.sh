2cfa21d6622ba4f8c4c4ad787c9d26d68ce5dede
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2020-11-10 17:43:27 -0300

    For clean up to reduce size of the images

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+. "$TESTSLIB/pkgdb.sh"
+
 clean_machine() {
-	find /var/log -type f -exec rm -f {} \;
+    distro_clean_package_cache
+
+    find /var/log -type f -exec rm -f {} \;
 
     rm -rf \
         /root/.bash_history \
