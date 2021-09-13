d4d3391be094a7c77f4beea48400de8d4bec123d
Sergio Cazzolato <sergio.cazzolato@canonical.com>
2021-09-13 16:21:06 -0300

    Add backups number for systems

--- lib/google_task.sh
+++ lib/google_task.sh
@@ -16,52 +16,62 @@ if [ "$GOOGLE_ACTION" = "add-image" ]; then
             SOURCE_SYSTEM=ubuntu-16.04-64
             TARGET_SYSTEM=amazon-linux-2-64
             RUN_SNAPD=false
+            BACKUPS=0
             ;;
         arch-linux)
             SOURCE_SYSTEM=ubuntu-16.04-64
             TARGET_SYSTEM=arch-linux-64-base
             RUN_SNAPD=false
+            BACKUPS=0
             ;;
         debian-sid)
             SOURCE_SYSTEM=debian-9-64-base
             TARGET_SYSTEM=debian-sid-base
             RUN_SNAPD=false
+            BACKUPS=0
             ;;
         fedora-27-64)
             # First added manually using a local fedora vm
             SOURCE_SYSTEM=fedora-26-64
             TARGET_SYSTEM=fedora-27-64-base
             RUN_SNAPD=false
+            BACKUPS=0
             ;;
         fedora-28-64)
             SOURCE_SYSTEM=fedora-27-64
             TARGET_SYSTEM=fedora-28-64-base
             RUN_SNAPD=false
+            BACKUPS=0
             ;;
         fedora-29-64)
             SOURCE_SYSTEM=fedora-28-64
             TARGET_SYSTEM=fedora-29-64-base
             RUN_SNAPD=false
+            BACKUPS=0
             ;;
         fedora-30-64)
             SOURCE_SYSTEM=fedora-29-64
             TARGET_SYSTEM=fedora-30-64-base
             RUN_SNAPD=false
+            BACKUPS=0
             ;;
         fedora-31-64)
             SOURCE_SYSTEM=fedora-30-64
             TARGET_SYSTEM=fedora-31-64-base
             RUN_SNAPD=false
+            BACKUPS=0
             ;;
         opensuse-42-2-64)
             SOURCE_SYSTEM=ubuntu-16.04-64
             TARGET_SYSTEM=opensuse-42.2-64-base
             RUN_SNAPD=false
+            BACKUPS=0
             ;;
         ubuntu-16.04-32)
             SOURCE_SYSTEM=ubuntu-16.04-64
             TARGET_SYSTEM=ubuntu-16.04-32-base
             RUN_SNAPD=false
+            BACKUPS=0
             ;;
         *)
         echo "ERROR: Unsupported distribution $SPREAD_SYSTEM"
@@ -74,121 +84,121 @@ elif [ "$GOOGLE_ACTION" = "update-image" ]; then
             SOURCE_SYSTEM=amazon-linux-2-64-base
             TARGET_SYSTEM=amazon-linux-2-64
             RUN_SNAPD=true
+            BACKUPS=0
             ;;
         arch-linux)
             SOURCE_SYSTEM=arch-linux-64-base
             TARGET_SYSTEM=arch-linux-64
             RUN_SNAPD=true
+            BACKUPS=0
             ;;
         centos-7)
             SOURCE_SYSTEM=centos-7-64-base
             TARGET_SYSTEM=centos-7-64
             RUN_SNAPD=true
+            BACKUPS=0
             ;;
         centos-8)
             SOURCE_SYSTEM=centos-8-64-base
             TARGET_SYSTEM=centos-8-64
             RUN_SNAPD=true
-            ;;
-        debian-9)
-            SOURCE_SYSTEM=debian-9-64-base
-            TARGET_SYSTEM=debian-9-64
-            RUN_SNAPD=true
+            BACKUPS=0
             ;;
         debian-10)
             SOURCE_SYSTEM=debian-10-64-base
             TARGET_SYSTEM=debian-10-64
             RUN_SNAPD=true
+            BACKUPS=0
             ;;
         debian-sid)
             SOURCE_SYSTEM=debian-sid-64-base
             TARGET_SYSTEM=debian-sid-64
             RUN_SNAPD=false
+            BACKUPS=1
             ;;
         fedora-33-64)
             SOURCE_SYSTEM=fedora-33-64-base
             TARGET_SYSTEM=fedora-33-64
             RUN_SNAPD=true
+            BACKUPS=0
             ;;
         fedora-34-64)
             SOURCE_SYSTEM=fedora-34-64-base
             TARGET_SYSTEM=fedora-34-64
             RUN_SNAPD=true
+            BACKUPS=0
             ;;
         fedora-rawhide-64)
             SOURCE_SYSTEM=fedora-rawhide-64
             TARGET_SYSTEM=fedora-rawhide-64
             RUN_SNAPD=false
-            ;;
-        opensuse-15-1)
-            SOURCE_SYSTEM=opensuse-15.1-64-base
-            TARGET_SYSTEM=opensuse-15.1-64
-            RUN_SNAPD=true
+            BACKUPS=1
             ;;
         opensuse-15-2)
             SOURCE_SYSTEM=opensuse-15.2-64-base
             TARGET_SYSTEM=opensuse-15.2-64
             RUN_SNAPD=true
+            BACKUPS=0
             ;;
         opensuse-15-3)
             SOURCE_SYSTEM=opensuse-15.3-64-base
             TARGET_SYSTEM=opensuse-15.3-64
             RUN_SNAPD=true
+            BACKUPS=0
             ;;
         opensuse-tumbleweed)
             SOURCE_SYSTEM=opensuse-tumbleweed-64-base
             TARGET_SYSTEM=opensuse-tumbleweed-64
             RUN_SNAPD=false
-            ;;
-        opensuse-tumbleweed-2)
-            SOURCE_SYSTEM=opensuse-tumbleweed-2-64-base
-            TARGET_SYSTEM=opensuse-tumbleweed-2-64
-            RUN_SNAPD=false
+            BACKUPS=1
             ;;
         ubuntu-14.04-64)
             SOURCE_SYSTEM=ubuntu-14.04-64
             TARGET_SYSTEM=ubuntu-14.04-64
             RUN_SNAPD=true
-            ;;
-        ubuntu-16.04-32)
-            SOURCE_SYSTEM=ubuntu-16.04-32-base
-            TARGET_SYSTEM=ubuntu-16.04-32
-            RUN_SNAPD=true
+            BACKUPS=1
             ;;
         ubuntu-16.04-64)
             SOURCE_SYSTEM=ubuntu-16.04-64-base
             TARGET_SYSTEM=ubuntu-16.04-64
             RUN_SNAPD=true
+            BACKUPS=1
             ;;
         ubuntu-18.04-32)
             SOURCE_SYSTEM=ubuntu-18.04-32-base
             TARGET_SYSTEM=ubuntu-18.04-32
             RUN_SNAPD=true
+            BACKUPS=1
             ;;
         ubuntu-18.04-64)
             SOURCE_SYSTEM=ubuntu-18.04-64-base
             TARGET_SYSTEM=ubuntu-18.04-64
             RUN_SNAPD=true
+            BACKUPS=0
             ;;            
         ubuntu-20.04-64)
             SOURCE_SYSTEM=ubuntu-20.04-64-base
             TARGET_SYSTEM=ubuntu-20.04-64
             RUN_SNAPD=true
+            BACKUPS=0
             ;;
         ubuntu-20.10-64)
             SOURCE_SYSTEM=ubuntu-20.10-64-base
             TARGET_SYSTEM=ubuntu-20.10-64
             RUN_SNAPD=true
+            BACKUPS=0
             ;;
         ubuntu-21.04-64)
             SOURCE_SYSTEM=ubuntu-21.04-64-base
             TARGET_SYSTEM=ubuntu-21.04-64
             RUN_SNAPD=true
+            BACKUPS=0
             ;;
         ubuntu-21.10-64)
             SOURCE_SYSTEM=ubuntu-21.10-64-external
             TARGET_SYSTEM=ubuntu-21.10-64
             RUN_SNAPD=false
+            BACKUPS=0
             ;;
         *)
             echo "ERROR: Unsupported distribution $SPREAD_SYSTEM"
