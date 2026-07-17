23d15dc665ac5cc265a8cf4cb658bea8f7cf2a7e
Maciej Borzecki <maciej.borzecki@canonical.com>
2026-07-17 11:15:21 +0200

    lib/pkgdb: appropriate commands for upgrading the distro on Fedora and openSUSE (#98)
    * lib/pkgdb: use zypper dup for distro upgrade instead of refresh
    
    zypper dup should be used for whole-system upgrades
    
    Signed-off-by: Maciek Borzecki <maciej.borzecki@canonical.com>
    
    * lib/pkgdb: use dnf distro-sync on Fedora
    
    Signed-off-by: Maciek Borzecki <maciej.borzecki@canonical.com>
    
    ---------
    
    Signed-off-by: Maciek Borzecki <maciej.borzecki@canonical.com>

--- lib/pkgdb.sh
+++ lib/pkgdb.sh
@@ -318,10 +318,10 @@ distro_upgrade_packages() {
             apt upgrade -y
             ;;
         fedora-*)
-            dnf upgrade --nogpgcheck -y
+            dnf distro-sync --nogpgcheck -y
             ;;
         opensuse-*)
-            zypper update -y --force-resolution --no-recommends --replacefiles
+            zypper dup -y --force-resolution --no-recommends --replacefiles
             ;;
         arch-*)
             pacman --needed --noconfirm -Syu
